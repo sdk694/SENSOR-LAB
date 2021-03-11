0fdf68b144669ab4c9447494d55d7ab714d2ab5e
Shreyas <sdk694@gmail.com>
2021-03-12 02:58:49 +0530

    deleted page and 8slphp

--- page/8SQLconnection.php
+++
@@ -1,8 +0,0 @@
-<?php
-        $link = mysqli_connect("localhost","root","","user"); 
-
-        if(mysqli_connect_error()){
-            die("Not connected to MySQL Database. Error! :("."<br>");
-        }
-
-?>
