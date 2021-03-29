d8792369205662e63918d26f7b3e819f08302c87
Shreyas <sdk694@gmail.com>
2021-03-29 13:11:56 +0530

    calculator navbar and color scheme chnaged

--- view_item2.php
+++ view_item2.php
@@ -53,6 +53,7 @@
             <span class="navbar-toggler-icon"></span>
             </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a href="" class="pull-left"><img src="solarsurveiltransparent.png" alt="" height="40px" style="margin-right: 12px; margin-top: 2px;"></a>
             <a class="navbar-brand" href="index.html">SOLAR SURVEIL</a>
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
             <li class="nav-item">
