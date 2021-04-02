7df62429989168994a4a9d71d44cf31ad0569e31
Shreyas <sdk694@gmail.com>
2021-04-03 02:16:51 +0530

    text-white in login and signup

--- register.php
+++ register.php
@@ -171,7 +171,7 @@
         /* height: 100%; */
         position: absolute;
         padding: 90px 70px 50px 70px;
-        background: #173f5f;
+        background: #3caea3;
     }
 
     .login-snip .login,
