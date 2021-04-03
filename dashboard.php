9b29733c3def62e0ced51b065de8fe4034c4e5d1
Shreyas <sdk694@gmail.com>
2021-04-03 19:12:29 +0530

    devices.php

--- dashboard.php
+++ dashboard.php
@@ -55,7 +55,7 @@ if(array_key_exists("email",$_SESSION)){
         <div class="dashboard-header">
             <nav class="navbar navbar-expand-lg fixed-top" id="navbar1">
                 <a href="index.html" class=""><img src="solarsurveiltransparent.png" alt="" height="40px" style="margin-left: 10px;"></a>
-                <a class="navbar-brand text-light" href="index.html" style=" font-family: Montserrat, sans-serif;">Solar Surveil</a>
+                <a class="navbar-brand text-light" href="index.html">SOLAR SURVEIL</a>
                     </ul>
                 </div>
             </nav>
