8b605f10011ade348145be49a218a4cfdaed2bcf
Shreyas <sdk694@gmail.com>
2021-04-03 23:59:01 +0530

    navbar

--- dashboard.php
+++ dashboard.php
@@ -53,7 +53,7 @@ if(array_key_exists("email",$_SESSION)){
         <!-- navbar -->
         <!-- ============================================================== -->
         <div class="dashboard-header">
-            <nav class="navbar navbar-expand-lg fixed-top" id="navbar1">
+            <nav class="navbar navbar-expand-lg fixed-top" id="navbar1" style ="margin-left = auto">
                 <a href="index.html" class=""><img src="solarsurveiltransparent.png" alt="" height="40px" style="margin-left: 10px;"></a>
                 <a class="navbar-brand text-white" href="index.html" style ="font-family:  Montserrat, sans-serif; letter-spacing: 4px; font-weight: 500; font-size: 20px;">SOLAR SURVEIL</a>
                     </ul>
--- devices.php
+++ devices.php
@@ -75,7 +75,7 @@ if(array_key_exists("email",$_SESSION)){
         <!-- navbar -->
         <!-- ============================================================== -->
         <div class="dashboard-header">
-            <nav class="navbar navbar-expand-lg fixed-top" id="navbar1" style = "min-height: 60px;">
+            <nav class="navbar navbar-expand-lg fixed-top" id="navbar1" style = "min-height: 60px; margin-left: auto">
                 <a href="index.html" class=""><img src="solarsurveiltransparent.png" alt="" height="40px" style="margin-left: 10px;"></a>
                 <a class="navbar-brand   text-light" href="index.html" style="font-family: Montserrat, sans-serif; letter-spacing: 4px; font-weight: 500; font-size: 20px;">SOLAR SURVEIL</a>
                     </ul>
