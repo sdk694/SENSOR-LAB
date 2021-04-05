77a157415852ea017a832bfb9a73c8a224f8820c
Shreyas <sdk694@gmail.com>
2021-04-06 01:49:08 +0530

    dashboard  updated

--- dashboard.php
+++ dashboard.php
@@ -66,7 +66,7 @@ if(array_key_exists("email",$_SESSION)){
         <!-- ============================================================== -->
         <div class="nav-left-sidebar sidebar-dark">
             <div class="menu-list">
-                <nav class=" navbar-expand-lg navbar-light">
+                <nav class="navbar navbar-expand-lg navbar-light">
                     <a class="d-xl-none d-lg-none" href="#">Dashboard</a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
