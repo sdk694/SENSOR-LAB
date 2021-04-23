bffd622d466d1ea5fe31058f569378cd9870015b
Shreyas <sdk694@gmail.com>
2021-04-23 18:24:28 +0530

    get components nav bar

--- dashboard.php
+++ dashboard.php
@@ -25,7 +25,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <!-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> -->
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha484-Gn5484xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E264XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -176,7 +176,7 @@
                                 });
                                 console.log(nodeDetails);
 
-                                fetch(" http://solarsurveildbit.in:1880/nodedata")
+                                fetch("http://solarsurveildbit.in:1880/nodedata")
                                 .then(response => response.json())
                                 .then(data => {
                                     console.log(data);
